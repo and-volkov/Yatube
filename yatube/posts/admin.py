20edbf2cc5f47bcce42e521f5088da5d5350e69b
Андрей Волков <andrey.dreamwall@gmail.com>
2022-04-18 16:59:13 +0300

    create admin.ModelAdmin for models Group, Comment

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from .models import Comment, Follow, Group, Post
 
 
-@admin.register(Post)  # added register decorator
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('pk', 'text', 'pub_date', 'author', 'group',)
     list_editable = ('group',)
@@ -13,11 +13,25 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = settings.EMPTY_VALUE_DISPLAY
 
 
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'description')
+    list_editable = ('title', 'description')
+    search_fields = ('title',)
+    list_filter = ('title',)
+    empty_value_display = settings.EMPTY_VALUE_DISPLAY
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'author', 'created', 'post')
+    list_editable = ('text',)
+    search_fields = ('author', 'text')
+    list_filter = ('created',)
+    empty_value_display = settings.EMPTY_VALUE_DISPLAY
+
+
 @admin.register(Follow)
 class FollowAdmin(admin.ModelAdmin):
-    list_display = ('user', 'author',)
+    list_display = ('pk', 'user', 'author',)
     list_display_links = ('user', 'author')
-
-
-admin.site.register(Group)
-admin.site.register(Comment)
