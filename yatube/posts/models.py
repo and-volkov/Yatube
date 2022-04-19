4759e285fa15bab604151ac05d428356d7ee03c5
Андрей Волков <andrey.dreamwall@gmail.com>
2022-04-19 18:55:06 +0300

    fix imports

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models import CheckConstraint, UniqueConstraint, Q, F
+from django.db.models import CheckConstraint, Q, F
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
