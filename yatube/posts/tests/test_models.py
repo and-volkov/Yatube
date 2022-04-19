e338cee859629a6777cc4403765c4524c1901889
Андрей Волков <andrey.dreamwall@gmail.com>
2022-04-19 18:54:12 +0300

    create tests for CommentModel

--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -52,8 +52,12 @@ class FollowModelTest(TestCase):
             'posts:profile_follow',
             kwargs={'username': self.USER_NAME}
         )
-        self.user_client.get(follow_page)
+        response = self.user_client.get(follow_page)
         self.assertEqual(Follow.objects.count(), 0)
+        self.assertRedirects(response, reverse(
+                                'posts:profile',
+                                kwargs={'username': self.USER_NAME})
+                             )
 
     def test_follow_object_unique(self):
         """Check follow objects have different pk"""
@@ -66,3 +70,41 @@ class FollowModelTest(TestCase):
             author=self.user
         )
         self.assertNotEqual(first_object.pk, second_object.pk)
+
+
+class CommentModelTest(TestCase):
+    USER_NAME = 'NoName'
+    COMMENT_AUTHOR = 'Commentator'
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username=cls.USER_NAME)
+        cls.commentator = User.objects.create_user(username=cls.COMMENT_AUTHOR)
+        cls.group = Group.objects.create(
+            title='Тестовая группа',
+            slug=GROUP_SLUG,
+            description='Тестовое описание',
+        )
+        cls.post = Post.objects.create(
+            author=cls.user,
+            text='Тестовый пост ' * 3,
+        )
+
+    def setUp(self):
+        self.commentator_client = Client()
+        self.commentator_client.force_login(self.commentator)
+        Comment.objects.create(
+            post=self.post,
+            author=self.user,
+            text='Comment test text' * 3,
+        )
+
+    def test_comment_created_successfully(self):
+        comments_count = Comment.objects.count()
+        self.assertEqual(comments_count, Comment.objects.count())
+
+    def test_str_func(self):
+        comment = Comment.objects.get(pk=1)
+        expected_comment_text = comment.text[:15]
+        self.assertEqual(expected_comment_text, str(comment))
